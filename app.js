fecc4b81027a03a4947b160a9ed48e579382bc3a
Pablo Plazuelo Jiménez <100439645+Kasi800@users.noreply.github.com>
2024-11-21 21:25:39 +0100

    Formulario empleados

--- app.js
+++ app.js
@@ -79,3 +79,14 @@ function initAutocomplete() {
         map.fitBounds(bounds);
     });
 }
+
+function rolEmpleado() {
+    let rol = document.getElementById("rol_empleado").value;
+    let div = document.getElementById("variable_rol");
+    
+    if (rol == "Cocinero") {
+        div.innerHTML = '<div><label for= "especialidad_cocinero" > Especialidad:</label ><select name="especialidad_cocinero" id="especialidad_cocinero"><option value="producto1">Producto1</option><option value="producto2">Producto2</option></select></div ><div><label for="jefe_cocinero">Cocinero jefe:</label><input type="checkbox" name="jefe_cocinero" id="jefe_cocinero"></div>';
+    } else if(rol == "Dependiente") {
+        div.innerHTML = '<label for="color_dependiente">Color:</label><select name="color_dependiente" id="color_dependiente"><option value="rojo">Rojo</option><option value="negro">Negro</option><option value="amarillo">Amarillo</option></select>';
+    }
+}
